0d379ca4eab04292bcc2b2e29fa5aea80a2a9232
Andre Evangelista <you@example.com>
2025-08-09 16:59:52 +0100

    add public supabase redirect

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -50,7 +50,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}/`,
+        redirectTo: process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URI || `${window.location.origin}/`,
       },
     })
     return { error }
